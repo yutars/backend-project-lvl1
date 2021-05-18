d120ccd4fe5b86ebf1e4dce84dc88f23f0cff65e
yutars <yutars@gmail.com>
2021-05-18 10:16:08 +0500

    upd9

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,18 +5,16 @@ import getRandomNum from '../utils.js';
 const description = 'What number is missing in the progression?';
 
 const generateRound = () => {
-  let geomProgression = [];
   const progLength = getRandomNum(5, 10);
   const hiddenElementIndex = getRandomNum(0, progLength - 1);
   const firstEl = getRandomNum(0, 100);
   const step = getRandomNum(1, 5);
-  geomProgression = _.range(progLength).map((element, index) => {
+  const geomProgression = _.range(progLength).map((element, index) => {
     if (index === hiddenElementIndex) return '..';
     return firstEl + element * step;
   });
   const question = geomProgression.join(' ');
   const answer = (firstEl + hiddenElementIndex * step).toString();
-  geomProgression = [];
   return [question, answer];
 };
 
